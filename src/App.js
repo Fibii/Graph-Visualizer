0e25a89d277aa0e3bcaa2edad43af0b1642a8830
Fibii <charafbahria@gmail.com>
2019-11-22 19:18:03 +0100

    added cytoscape library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
+import cytoscape from 'cytoscape';
+import './index.css'
 
+const nodes = [
+  {
+    data: {
+      id: 'A'
+    }
+  },
+  {
+    data:{
+      id:'B'
+    }
+  }
+
+];
 
 function App() {
+  const [cy, setCy] = useState(null);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    console.log(containerRef);
+    const cy = cytoscape({
+      container: containerRef.current, // container to render in
+
+      elements: nodes
+    });
+
+  });
+
+
   return (
-    <h1>hello</h1>
+      <div>
+        <h1>hello</h1>
+        <div ref={containerRef} id='cy'></div>
+      </div>
   );
 }
 
